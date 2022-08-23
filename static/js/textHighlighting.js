443591efa1438c40bca87aa85b6e91cc90da1e4f
qpwoei0123 <85989215+qpwoei0123@users.noreply.github.com>
2022-08-23 22:33:33 +0900

    텍스트 하이라이팅 및 에딧파일 부분 수정

--- static/js/textHighlighting.js
+++ static/js/textHighlighting.js
@@ -1,14 +1,18 @@
 //*변수 선언부*//
-const videoFile = document.getElementById("audio-editFile")          //오디오 바
+const audioFile = document.getElementById("audio-editFile")          //오디오 바
 const timeLine = document.getElementsByClassName("timeLine")        //시작-끝 시간
 const textSet = document.getElementsByClassName("newStartText")     //문장
 const textSetElement = document.getElementsByClassName("newStart")  //문자
+const fileTextNumbers = document.getElementById("fileTextNumbers")   //이미지 영역 아래 문장 수 div
 
 //*실행부*//
-videoFile.ontimeupdate = function () {now()}
+audioFile.ontimeupdate = function () {now()}
 //모튼 문자에 클릭 이벤트 부여
 for (i=0; i<textSetElement.length; i++) {
-    textSetElement.item(i).addEventListener("click", TextOnClick)
+    textSetElement.item(i).addEventListener("click", TextSetElementOnClick)
+}
+for (i=0; i<textSet.length; i++){
+    textSet.item(i).addEventListener("click", TextSetOnClick)
 }
 //문장 다시 그리기
 reText()
@@ -25,13 +29,13 @@ function now(){
     }
     //현재 위치 찾기
     for (i=0; i<timeLine.length; i++) {
-        const videoFileCopy = videoFile.currentTime
-        const timeLineEndTime = timeLine.item(i).lastElementChild.innerHTML / 1000
-        if (videoFileCopy < timeLineEndTime) {
+        const audioCurrentTime = audioFile.currentTime
+        const timeLineEndTime = textSet.item(i).lastElementChild.previousElementSibling.innerText / 1000
+        if (audioCurrentTime < timeLineEndTime) {
             timeLine.item(i).parentElement.parentElement.style.backgroundColor='grey'
             for (j=1; j<textSet.item(i).children.length; j+=3){    //[start, end, text]
-                const NewStartEndTime = textSet.item(i).children[j].innerHTML / 1000
-                if (videoFile.currentTime < NewStartEndTime){
+                const textSetElementEndTime = textSet.item(i).children[j].innerHTML / 1000
+                if (audioFile.currentTime < textSetElementEndTime){
                     textSet.item(i).children[j+1].style.color="blue"
                     break
                 }
@@ -40,19 +44,40 @@ function now(){
         }
     }
 }
+
 // [start, end, text] 형태로 출력 되기 때문에 text만 보이게하기
 function reText(){
     const textSetCount = textSet.length
+    fileTextNumbers.innerHTML="문장수: " + textSetCount.toString()
+
     for (i=0; i<textSetCount; i++){
         const textSetChildCount = textSet.item(i).childElementCount
         for(j=2; j<textSetChildCount; j+=3){   //[start, end, text]
             textSet.item(i).children[j].style.display="inline"
         }
+        //타임라인 다시그리기
+        let TimeLineStart = timeLine.item(i).firstElementChild
+        let TimeLineEnd = timeLine.item(i).lastElementChild
+        TimeLineStart.innerHTML = "Start: " + Math.floor(TimeLineStart.innerHTML /1000) + "s"
+        TimeLineEnd.innerHTML = "End: " + Math.floor(TimeLineEnd.innerHTML /1000) + "s"
+
     }
 }
 //단어 클릭시
-function TextOnClick(e) {
+function TextSetElementOnClick(e) {
     const textStartTime = e.target.previousElementSibling.previousElementSibling.innerHTML / 1000
-    videoFile.currentTime = textStartTime
-    videoFile.play()
+    audioFile.currentTime = textStartTime
+    audioFile.play()
+}
+
+//문장 영역 클릭시
+function TextSetOnClick(e){
+    if (e.target !== e.currentTarget){
+        return
+    }
+    else {
+        const textStartTime = e.target.firstElementChild.innerHTML / 1000
+        audioFile.currentTime = textStartTime
+        audioFile.play()
+    }
 }
